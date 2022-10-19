87f5c34b31d5adb8ec34bc96a70a89f079259fdf
Filipe Palheta <filipepalheta1@gmail.com>
2022-10-19 12:13:52 -0300

    Tipo de Transação

--- src/controllers/AssinaturaController.js
+++ src/controllers/AssinaturaController.js
@@ -4,12 +4,14 @@ class AssinaturaController {
     static createAssinatura = async (req, res) => {
         // const emails = req.body.Subscription.Customer.emails
         const dadosReq = req.body
+        const transactionType = req.body.event
+        console.log(transactionType)
 
-        if(dadosReq.Subscription){
-            console.log(dadosReq.Subscription.status)
-        }else{
-            console.log(dadosReq)
-        }
+        // if(dadosReq.Subscription){
+        //     console.log(dadosReq.Subscription.status)
+        // }else{
+        //     console.log(dadosReq)
+        // }
         res.json({success: true})
     }
 }
