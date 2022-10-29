63105300d8d6064ea82dec66cc80853a800d81bb
Filipe Palheta <filipepalheta1@gmail.com>
2022-10-29 16:14:34 -0300

    Initial Commit

--- src/config/conn.js
+++ src/config/conn.js
@@ -1,22 +1,22 @@
 import {Sequelize} from 'sequelize'
 
 
-const instance = new Sequelize(
-  'geral', 
-  'root', 
-  '', 
-  {
-    host: 'localhost',
-    dialect: 'mysql'
-})
-
 // const instance = new Sequelize(
-//   'palhet67_geral', 
-//   'palhet67_usuario', 
-//   '6m&G7ZPNh*69', 
-// {
-//     host: '108.179.193.193',
+//   'geral', 
+//   'root', 
+//   '', 
+//   {
+//     host: 'localhost',
 //     dialect: 'mysql'
 // })
+
+const instance = new Sequelize(
+  'palhet67_geral', 
+  'palhet67_usuario', 
+  '6m&G7ZPNh*69', 
+{
+    host: '108.179.193.193',
+    dialect: 'mysql'
+})
 
 export default instance
